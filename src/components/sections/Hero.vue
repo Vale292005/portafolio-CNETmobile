77975bd3696c450837f0ab7ee88a971ad651573c
Vale292005 <valeria.florezp@uqvirtual.edu.co>
2026-01-16 21:29:29 -0500

    re diseño con animacion termiando el header

--- src/components/sections/Hero.vue
+++ src/components/sections/Hero.vue
@@ -1,317 +1,211 @@
+<template>
+  <div class="pagina">
+    <header class="ImgHero">
+      <div class="content-img">
+        <img :src="hero"/>
+      </div>
+      <div :class="['content-frame', { 'is-animated': animated }]">
+        <div class="cent">
+          <div class="frame3">
+            <div class="img-logo">
+              <img :src="logo"/>
+            </div>
+            <h3>Este proyecto consiste en el rediseño del sitio web de CENT Mobile, un comparador de celulares, con el objetivo de reducir la tasa de deserción durante el proceso de comparación. Mi rol fue Product Designer, y el proyecto se desarrolló en un período de 4 semanas.</h3>
+          </div>
+          <img :src="phone">
+        </div>
+      </div>
+    </header>
+    <!-- Aparatado de De4sing Thinking-->
+    <div class="body">
+      <div class="desing-thinking">
+        <h3 class="title">Design Thinking</h3><h3>
+        Los usuarios presentaban dificultades para comparar los equipos seleccionados, lo que generaba frustración y aumentaba el abandono de la tarea. El principal objetivo del rediseño fue mejorar el flujo de comparación para hacerlo más claro, eficiente e intuitivo.
+      </h3>
+      </div>
+    <div class="conteiner-steps">
+      <div class="conteiner-steps-text">
+        <img :src="numero1"/>
+        <h3>En la etapa de <h3 class="highlight">empatizar</h3> cla se desarrollaron user personas y user journey maps para comprender los puntos desfavorables y necesidades de los usuarios dentro de la plataforma.</h3>
+      </div>
+      <div class="carruselImg">
+        <div class="carruselImg-track">
+          <img :src="userPersona1"/>
+          <img :src="userPersona2"/>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+
 <script setup>
-import heroImage from '../../assets/hero.png'
-import headerImg from '../../assets/iPhone 15 Pro - Black Flatten.png'
-import headerLogo from '../../assets/Logo.png'
-import numero1 from'../../assets/numero1.png'
-import numero2 from'../../assets/numero2.png'
-import userPersona1 from '../../assets/userPersona1.png'
-import userPersona2 from '../../assets/userPersona2.png'
-import tabla1 from '../../assets/tabla1.png'
-import tabla2 from '../../assets/tabla2.png'
-import numero3 from '../../assets/numero3.png'
-import userFlow from '../../assets/userFlow.png'
-import numero4 from '../../assets/numero4.png'
-const mislinks=[
-  {texto:"Benchmarking-Comparador de Celulares",url:"https://docs.google.com/spreadsheets/d/1u_ppicMli-fzyXzI0t9cM1sIDzzB1vupe9ciQfys_7s/edit?usp=sharing"},
-  {texto:"User Persona",url:"https://www.figma.com/board/2KGga2N3wx4sRHcnELcvPr/User-Persona?node-id=0-1&t=rJJ8ujbLBdoFw9Qe-1&fuid=1577445020502448024"},
-  {texto:"User JourneyMap",url:"https://www.figma.com/board/vg8CurVffdUdWHx2hPckVc/User-Journey-Map?node-id=0-1&t=PM3OosJf8Aqkl72C-1&fuid=1577445020502448024"},
-  {texto:"Diagrama de flujo",url:"https://www.figma.com/board/vg8CurVffdUdWHx2hPckVc/User-Journey-Map?node-id=0-1&t=PM3OosJf8Aqkl72C-1&fuid=1577445020502448024"},
-  {texto:"Prototipo",url:"https://www.figma.com/board/vg8CurVffdUdWHx2hPckVc/User-Journey-Map?node-id=0-1&t=PM3OosJf8Aqkl72C-1&fuid=1577445020502448024"}
-]
-const micontacto=[
-  {texto:"Linkedin",url:"https://www.linkedin.com/in/valeria-florez-paz-75ba0337b/"},
-  {texto:"Github",url:"https://www.linkedin.com/in/valeria-florez-paz-75ba0337b/"}
-]
+import {ref, onMounted} from 'vue';
+const animated =ref(false);
+onMounted(() =>{
+  setTimeout(() =>{
+    animated.value=true;
+  },200);
+})
+
+import hero from '../../assets/hero.png';
+import numero1 from '../../assets/numero1.png';
+import userPersona1 from '../../assets/userPersona1.png';
+import userPersona2 from '../../assets/userPersona2.png';
+import logo from '../../assets/Logo.png';
+import phone from '../../assets/iPhone 15 Pro - Black Flatten.png';
 </script>
 
-<template>
-
-  <section class="hero">
-
-    <!--Imagen superior -->
-    <div class="heroImage">
-      <img class="bg" :src="heroImage"/>
-    </div>
-
-    <!--header-->
-    <div class="headerSection">
-      <header class="header">
-        <div class="frame">
-          <img class="logo" :src="headerLogo" alt="Logo"/>
-          <p class="text"><b>CNET</b> es un portal global de noticias y reseñas tecnológicas. En este proyecto se realizó un rediseño <b>UX/UI</b> de la sección de <b>comparativa de celulares</b>, con el objetivo de mejorar el flujo de navegación y la <b>interacción del usuario</b> en la versión móvil.</p>
-        </div>
-        <img class="photo" :src="headerImg" alt="Foto CNET en movil" />
-      </header>
-
-      <div class="body">
-        <div class="contenedor">
-          <div class="contenedorNumeroyTexto">
-            <img class="numero" :src="numero1" alt="01"/>
-            <p class="h3">En la etapa de <b>empatizar</b> se desarrollaron user personas y user journey maps para comprender los puntos desfavorables y necesidades de los usuarios dentro de la plataforma.</p>
-          </div>
-          <div class="carrosel">
-            <img class="carrosel1" :src="userPersona1" alt="userPersona1"/>
-            <img class="carrosel2" :src="userPersona2" alt="userPersona2"/>
-          </div>
-        </div>
-        <div class="contenedor">
-          <div class="contenedorNumeroyTexto">
-            <img class="numero" :src="numero2" alt="02"/>
-            <p class="h3">En la etapa de <b>definir</b>, se identificó que el principal problema era un flujo de pasos deficiente para completar la tarea. Los usuarios no sabían cómo realizarla correctamente y, aun cuando lo lograban, no comprendían cómo lo habían hecho, lo que generaba frustración y abandono de la plataforma.</p>
-          </div>
-          <div class="carrosel">
-            <img class="tabla" :src="tabla1" alt="grafico">
-            <img class="tabla" :src="tabla2" alt="grafico">
-          </div>
-        </div>
-        <div class="contenedor">
-          <div class="contenedorNumeroyTexto">
-            <img class="numero" :src="numero3" alt="03"/>
-            <p class="h3">En la etapa de <b>idear</b>, se creó un mapa de flujo que facilita el recorrido del usuario, junto con el diseño de botones y una interfaz más intuitiva, reduciendo opciones innecesarias para que el usuario pueda completar su camino sin distracciones.</p>
-          </div>
-          <div class="contenedorImg">
-            <img class="userFlow" :src="userFlow" alt="userFlow"/>
-          </div>
-        </div>
-        <div class="contenedorBlack">
-          <div class="contenedorNumeroyTexto">
-            <img class="numero" :src="numero4" alt="04"/>
-            <p class="h3Black">En la etapa de <b>prototipar</b>, se diseñó la interfaz utilizando los colores principales de la página para el logo y los botones, aportando un aspecto tecnológico y coherente con la identidad de la marca.</p>
-          </div>
-          <div class="contenedorImg">
-            <a href="https://www.figma.com/design/c9c14N0b7nE9twxRLOyMWI/CNET-mobile?node-id=20-274&t=QguBa9XnPy4J1Z1z-1" rel="noopener noreferrer">
-              <img class="photo" :src="headerImg" alt="celular"/>
-            </a>
-          </div>
-        </div>
-        <div class="contenedorInfo">
-          <div class="referencias">
-            <p class="h3Black"><b>Referencias:</b></p>
-            <ul class="lista-custom">
-              <li v-for="link in mislinks" :key="link.texto">
-                <a :href="link.url" target="_blanck" rel="noopener noreferrer">
-                  {{link.texto}}
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="contacto">
-            <p class="h3Black"><b>Contacto:</b></p>
-            <ul class="lista-custom">
-              <li v-for="link in micontacto" :key="link.texto">
-                <a :href="link.url" target="_blank" rel="noopener noreferrer">
-                  {{link.texto}}
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <div class="links">
-      </div>
-    </div>
-  </section>
-</template>
 <style scoped>
-/* Importación desde Google Fonts */
-@import url('https://fonts.googleapis.com/css2?family=Schibsted+Grotesk:ital,wght@0,400..900;1,400..900&display=swap');
-.hero {
+.pagina {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+
+  width: 100%;
+  height: auto;
+
+  background: #1d1d27;
+}
+.content-img {
+  width: 100%;
+  height: 100vh;
+  background: #1d1d27;
+}
+.content-img img{
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  filter: blur(3px);
+}
+.content-frame {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  top: 0;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: auto;
+
+  position: absolute;
+
+  transform: translateY(100vh);
+
+  transition: transform 1.2s cubic-bezier(0.19,1,0.22,1);
+  will-change: transform;
+
+}
+.content-frame.is-animated {
+  transform: translateY(23vh);
+}
+.cent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: auto;
+  height: auto;
+
+  gap: 20px;
+  padding: 40px;
+  background: #1d1d27;
+}
+.cent img {
+  width: 201px;
+  height: 413px;
+}
+.img-logo {
   width: 100%;
 }
-/* Imagen Superior*/
-.heroImage img{
-  width: 100%;
+
+.img-logo img {
+  width: 601px;
+  max-width: 100%;
   height: auto;
   display: block;
 }
-/* headerSeccion*/
-/* Seccion con fondo*/
-.headerSection{
-  background: #1D1D27;
-  background: linear-gradient(180deg ,#1D1D27 86%,black 91%);
-  width: 100%;
-}
-.header{
-  max-width: 1280px;
-  width: 100%;
-  margin: 0 auto;
-  height: 670px;
 
-  display:flex;
-  align-items: center;
-  justify-content: center;
-  gap: clamp(40px, 9vw, 120px);
-
-  color: aliceblue;
-}
-
-/*Bloque de texto*/
-.frame{
-  width: 600px;
-  height: 490px;
-
+.frame3 {
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  gap: 24px;
+  align-items: flex-start;
+  justify-content: center;
+
+  width: auto;
+  height: auto;
+
+  gap: 10px;
+
+}
+.frame3 h3 {
+  width: auto;
+  max-width: 601px;
+  height: auto;
+  margin: 0;
 }
 
-/*Logo*/
-.logo{
+/*Apartado Desing Thinkin*/
+.body {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+
   width: 100%;
   height: auto;
-  object-fit: contain;
-}
 
-/*Testp*/
-.text{
-  font-size: 1.1rem;
-  line-height: 1.7;
-  opacity: 0.9;
-}
-
-/*Photo*/
-.photo {
-  width: clamp(180px, 21vw, 254px);
-  height: clamp(360px, 43vw, 521px);
-  object-fit: cover;
-  border-radius: 20px;
-}
-
-
-/*body*/
-.contenedor{
-  width: calc(100%- 40px);
-  margin: 0 auto;
-  background: #D9D9D9;
-  padding: 20px;
-  margin-left: 20px;
-  margin-right: 20px;
   gap: 20px;
+  padding-inline: 40px;
+  padding-top: 100px;
 }
-/*texto superior*/
-.contenedorNumeroyTexto{
+.desing-thinking {
+  justify-items: center;
+  max-width: 1000px;
+}
+.conteiner-steps {
   display: flex;
-  align-items: flex-start;
-  gap: 10px;
-  justify-content: flex-start;
-  font-family: 'Schibsted Grotesk',sans-serif;
-  margin-bottom: 20px;
+  flex-direction: column;
+  justify-items: start;
+  align-items: center;
+
+  width: 100%;
+  height: auto;
+
+  padding: 20px;
+
+  background: #d9d9d9;
 }
-.numero{
-  width: 30px;
-  height: 30px;
-  flex-shrink: 0;
-}
-.h3{
-  margin: 0;
-  font-size: 22px;
-  line-height: 1.2;
+.conteiner-steps-text h3 {
   color: #1d1d27;
 }
-/* carroselUserPersonas*/
-.carrosel{
-  display: flex;
-  gap: 20px;
+.conteiner-steps-text img{
+  width: 26px;
+  height: 27px;
+}
+.carruselImg {
+  width: 100%;
   overflow-x: auto;
-  scroll-behavior: smooth;
+  scroll-snap-type: x mandatory;
+  -webkit-overflow-scrolling: touch;
 
   scrollbar-width: none;
-  -ms-overflow-style: none;
 }
-.carrosel::-webkit-scrollbar{
+.carruselImg::-webkit-scrollbar {
   display: none;
 }
-.carrosel img{
-  flex-shrink: 0;
-  width: auto;
-  height: 600px;
-  object-fit: contain;
-}
-
-.contenedorImg{
+.carruselImg-track {
   display: flex;
-  justify-content: center;
-  width: 100%;
-}
-.userFlow{
-  flex-shrink: 0;
-  height: 1500px;
-  width: auto;
-}
-
-.contenedorBlack{
-  width: calc(100%- 40px);
-  margin: 0 auto;
-  gap: 20px;
-  padding: 20px;
-  height: auto;
-}
-.h3Black{
   margin: 0;
-  font-size: 22px;
-  line-height: 1.2;
-  color: #ffffff;
-}
-
-
-/* 1. Contenedor principal (ul) */
-.lista-custom {
-  list-style: none;
   padding: 0;
-  margin: 0;
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
 }
-
-/* 2. Todos los estados del enlace (a) para eliminar el azul y el morado */
-.lista-custom li a,
-.lista-custom li a:link,
-.lista-custom li a:visited {
-  color: #FFFFFF !important; /* Texto siempre blanco */
-  text-decoration: none !important;
-  font-family: 'Schibsted Grotesk', sans-serif;
-  font-size: 1.1rem;
-  font-weight: 500;
-  transition: all 0.3s ease;
-  display: inline-block;
-}
-
-/* 3. Efecto Hover (Rosa EA5FE9) */
-.lista-custom li a:hover {
-  color: #EA5FE9 !important; /* Tu color rosa */
-  transform: translateX(8px); /* Se mueve a la derecha */
-  text-decoration: underline !important;
-}
-
-/* 4. Estado activo (cuando haces clic) */
-.lista-custom li a:active {
-  color: #EA5FE9 !important;
-}
-
-.contenedorInfo{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 512px;
-  width: auto;
-  gap: 10px;
-}
-.referencias{
-  justify-content: center;
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  width:100%;
-}
-.contacto{
-  justify-content: center;
-  align-items: center;
-  display: flex;
-  flex-direction: column;
+.carruselImg img {
   width: 100%;
+  flex: 0 0 100%;
+  height: auto;
+  object-fit: contain;
+  scroll-snap-align: start;
+  display: flex;
 }
-
 </style>

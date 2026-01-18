7f7afd28b2d7733bf42f491b72aa211b075d3879
Vale292005 <valeria.florezp@uqvirtual.edu.co>
2026-01-17 22:43:38 -0500

    completo hasta el paso 3

--- src/components/sections/Hero.vue
+++ src/components/sections/Hero.vue
@@ -26,15 +26,38 @@
     <div class="conteiner-steps">
       <div class="conteiner-steps-text">
         <img :src="numero1"/>
-        <h3>En la etapa de <h3 class="highlight">empatizar</h3> cla se desarrollaron user personas y user journey maps para comprender los puntos desfavorables y necesidades de los usuarios dentro de la plataforma.</h3>
+        <h3>En la etapa de <span class="highlight">empatizar</span> se desarrollaron user personas y user journey maps para comprender los puntos desfavorables y necesidades de los usuarios dentro de la plataforma.</h3>
       </div>
       <div class="carruselImg">
         <div class="carruselImg-track">
-          <img :src="userPersona1"/>
-          <img :src="userPersona2"/>
+          <img :src="img"/>
+          <img :src="img1"/>
+          <img :src="img2"/>
+          <img :src="img3"/>
         </div>
       </div>
     </div>
+      <div class="conteiner-steps">
+        <div class="conteiner-steps-text">
+          <img :src="numero2"/>
+          <h3>En la etapa de <span class="highlight">definir</span>, se identificó que el principal problema era un flujo de pasos deficiente para completar la tarea. Los usuarios no sabían cómo realizarla correctamente y, aun cuando lo lograban, no comprendían cómo lo habían hecho, lo que generaba frustración y abandono de la plataforma.</h3>
+        </div>
+        <div class="carruselImg">
+          <div class="carruselImg-track">
+            <img :src="img4"/>
+            <img :src="img5"/>
+          </div>
+        </div>
+      </div>
+      <div class="conteiner-steps">
+        <div class="conteiner-steps-text">
+          <img :src="numero2"/>
+          <h3>En la etapa de <span class="highlight">idear</span>, se creó un mapa de flujo que facilita el recorrido del usuario, junto con el diseño de botones y una interfaz más intuitiva, reduciendo opciones innecesarias para que el usuario pueda completar su camino sin distracciones.</h3>
+        </div>
+        <div class="imgUserFlow">
+          <img :src="img6"/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -51,8 +74,16 @@ onMounted(() =>{
 
 import hero from '../../assets/hero.png';
 import numero1 from '../../assets/numero1.png';
-import userPersona1 from '../../assets/userPersona1.png';
-import userPersona2 from '../../assets/userPersona2.png';
+import numero2 from '../../assets/numero2.png';
+import numero3 from '../../assets/numero3.png';
+import numero4 from '../../assets/numero4.png';
+import img from '../../assets/img.png';
+import img1 from '../../assets/img_1.png';
+import img2 from '../../assets/img_2.png';
+import img3 from '../../assets/img_3.png';
+import img4 from '../../assets/img_4.png';
+import img5 from '../../assets/img_5.png';
+import img6 from '../../assets/img_6.png';
 import logo from '../../assets/Logo.png';
 import phone from '../../assets/iPhone 15 Pro - Black Flatten.png';
 </script>
@@ -69,22 +100,31 @@ import phone from '../../assets/iPhone 15 Pro - Black Flatten.png';
 
   background: #1d1d27;
 }
+.ImgHero {
+  position: relative;
+  width: 100%;
+  height: auto;
+  min-height: 652px;
+}
 .content-img {
   width: 100%;
-  height: 100vh;
+  height: 100%;
   background: #1d1d27;
+
+  min-height: 652px;
 }
 .content-img img{
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   object-fit: cover;
-  filter: blur(3px);
+
+  min-height: 652px;
 }
 .content-frame {
   margin: 0 auto;
   display: flex;
   flex-direction: row;
-  top: 0;
+  bottom: 0;
   align-items: center;
   justify-content: center;
   width: 100%;
@@ -92,14 +132,14 @@ import phone from '../../assets/iPhone 15 Pro - Black Flatten.png';
 
   position: absolute;
 
-  transform: translateY(100vh);
+  transform: translateY(100%);
 
   transition: transform 1.2s cubic-bezier(0.19,1,0.22,1);
   will-change: transform;
 
 }
 .content-frame.is-animated {
-  transform: translateY(23vh);
+  transform: translateY(0);
 }
 .cent {
   display: flex;
@@ -174,38 +214,53 @@ import phone from '../../assets/iPhone 15 Pro - Black Flatten.png';
   height: auto;
 
   padding: 20px;
-
+  gap: 20px;
   background: #d9d9d9;
+}
+.conteiner-steps-text {
+  display: flex;
+  flex-direction: row;
+  margin: 0;
 }
 .conteiner-steps-text h3 {
   color: #1d1d27;
+}
+.highlight{
+  font-weight: 600; /* SemiBold */
+  font-style: italic;
 }
 .conteiner-steps-text img{
   width: 26px;
   height: 27px;
 }
 .carruselImg {
-  width: 100%;
   overflow-x: auto;
   scroll-snap-type: x mandatory;
   -webkit-overflow-scrolling: touch;
-
   scrollbar-width: none;
+  width: 1000px;
 }
 .carruselImg::-webkit-scrollbar {
   display: none;
 }
 .carruselImg-track {
-  display: flex;
-  margin: 0;
-  padding: 0;
+  display: inline-flex;
+
+  gap: 20px;
 }
 .carruselImg img {
-  width: 100%;
-  flex: 0 0 100%;
-  height: auto;
+  flex: 0 0 auto;
+  height: 500px;
   object-fit: contain;
   scroll-snap-align: start;
-  display: flex;
+  display: block;
+}
+.imgUserFlow {
+  width: 100%;
+  height: auto;
+}
+.imgUserFlow img{
+  width: 100%;
+  object-fit: contain;
 }
 </style>
